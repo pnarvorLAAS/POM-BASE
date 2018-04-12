ab7db51db96be5624c94df0387eb8eca9e65de17
Pierre Narvor <pnarvor@laas.fr>
2018-04-12 12:28:14 +0200

    (Splitted from CDFF_at_LAAS)[Crisp-rebase] Add PositionManagerBase

---
+++ include/PositionManagerBase.hpp
@@ -0,0 +1,157 @@
+#ifndef DEF_POSITIONMANAGER_POSITIONMANAGERBASE_H
+#define DEF_POSITIONMANAGER_POSITIONMANAGERBASE_H
+
+#include <iostream>
+#include <sys/time.h>
+#include <string>
+
+#include <envire_core/graph/EnvireGraph.hpp>
+
+namespace PositionManager
+{
+
+typedef envire::core::FrameId FrameId; //Is a std::string
+typedef envire::core::Transform Transform;
+typedef base::Matrix6d Covariance;
+typedef long long TimeUs;
+
+class TimeManager
+{
+    public:
+
+    static const unsigned int USEC_PER_SEC = 1000000;
+
+    static TimeUs now()
+    {
+        timeval tv;
+        gettimeofday(&tv,NULL);
+
+        return USEC_PER_SEC*tv.tv_sec + tv.tv_usec;
+    }
+
+    static std::string toString(TimeUs time)
+    {
+        char str[512];
+        int i;
+        timeval tv;
+        struct tm timeinfo;
+        
+        tv.tv_sec = time / USEC_PER_SEC;
+        tv.tv_usec = time % USEC_PER_SEC;
+        
+        localtime_r(&tv.tv_sec, &timeinfo);
+        i = strftime(str, 512, "%F %T.", &timeinfo);
+        sprintf(str + i, "%6ld", tv.tv_usec);
+
+        return std::string(str);
+    }
+    
+    static std::string toStringShort(TimeUs time)
+    {
+        char str[512];
+        int i;
+        timeval tv;
+        struct tm timeinfo;
+        
+        tv.tv_sec = time / USEC_PER_SEC;
+        tv.tv_usec = time % USEC_PER_SEC;
+        
+        localtime_r(&tv.tv_sec, &timeinfo);
+        i = strftime(str, 512, "%T.", &timeinfo);
+        sprintf(str + i, "%6ld", tv.tv_usec);
+
+        return std::string(str);
+    }
+};
+
+class Pose 
+{
+    public :
+
+    Pose() {}
+    Pose(const FrameId& parent, const FrameId& child, const Transform& tr) : 
+        _parent(parent), _child(child), _tr(tr), _childTime(-1), _parentTime(-1) {}
+    Pose(const Pose& pose) : 
+        _parent(pose._parent), _parentTime(pose._parentTime),  _child(pose._child), _childTime(pose._childTime), _tr(pose._tr) {}
+
+    std::string toString() const
+    { 
+        return _parent + "->" + _child + "\n" + this->toStringShort(_tr);
+    }
+
+    std::string toStringVerboseShort() const
+    {
+        std::ostringstream stream;
+        stream << _parent << " " << PositionManager::TimeManager::toStringShort(_parentTime) 
+               << "\n->\n"
+               << _child  << " " << PositionManager::TimeManager::toStringShort(_parentTime)
+               << "\n" << this->toStringShort(_tr);
+        return stream.str();
+    }
+
+    std::string toStringVerbose() const
+    {
+        std::ostringstream stream;
+        stream << _parent <<" "<< PositionManager::TimeManager::toString(_parentTime)
+               << "\n->\n"
+               << _child  <<" "<< PositionManager::TimeManager::toString(_parentTime)
+               << "\n" << this->toString(_tr) << "\n" << _tr.transform.cov;
+
+        return stream.str();
+    }
+
+    std::string toString(const Transform& tr) const
+    {
+        std::ostringstream oss;
+        oss << "t: (" << tr.transform.translation(0) << " " 
+                      << tr.transform.translation(1) << " " 
+                      << tr.transform.translation(2) << ")\n"
+            << "r: (" << tr.transform.orientation.w() << " " 
+                      << tr.transform.orientation.x() << " " 
+                      << tr.transform.orientation.y() << " " 
+                      << tr.transform.orientation.z() << ")"; 
+
+        return oss.str();
+    }
+    
+    std::string toStringShort(const Transform& tr) const
+    {
+        std::ostringstream oss;
+        oss.setf(std::ios::fixed, std::ios::floatfield);
+        oss.precision(2);
+        
+        oss << "t: (" << tr.transform.translation(0) << " " 
+                      << tr.transform.translation(1) << " " 
+                      << tr.transform.translation(2) << ")\n"
+            << "r: (" << tr.transform.orientation.w() << " " 
+                      << tr.transform.orientation.x() << " " 
+                      << tr.transform.orientation.y() << " " 
+                      << tr.transform.orientation.z() << ")"; 
+
+        return oss.str();
+    }
+
+    FrameId _parent;
+    TimeUs _parentTime;
+    FrameId _child;
+    TimeUs _childTime;
+    Transform _tr;
+};
+
+class Graph : public envire::core::EnvireGraph
+{
+    // write accessors for vertices
+
+    public:
+
+    Graph();
+
+    void copyFrom(Graph& src);
+    // set the rootnode name to avoid root node reaturn in leaves if the root node has only one child. (In an undirected tree, the root node is undefined)
+    std::list<FrameId> getLeaves(const PositionManager::FrameId* rootNode = NULL);
+};
+
+};
+
+#endif
+
---
+++ src/PositionManagerBase.cpp
@@ -0,0 +1,61 @@
+#include "PositionManagerBase.hpp"
+
+using namespace PositionManager;
+
+Graph::Graph()
+{
+}
+
+void Graph::copyFrom(Graph& src)
+{
+    this->graph().clear();
+    boost::copy_graph(src, this->graph());
+    this->regenerateLabelMap();
+}
+
+std::list<FrameId> Graph::getLeaves(const PositionManager::FrameId* rootNode)
+{
+    // find nodes with no child
+    // warning : there are always edges in both directions even for a directed graph. A leaf is therefore a node with only one child (its parent)
+
+    std::list<FrameId> leafList;
+    std::map<FrameId, unsigned int> nodeMap; //frame id + number of children
+
+    typename boost::graph_traits<EnvireGraph>::vertex_iterator it, end;
+    for(boost::tie(it,end) = boost::vertices(this->graph()); it != end; ++it)
+    {
+        nodeMap.insert(std::pair<FrameId,unsigned int>(getFrameId(*it),0));
+    }
+
+    // counting number of children for each node
+    std::pair<EnvireGraph::edge_iterator, EnvireGraph::edge_iterator> edges_it = this->getEdges();
+    for(EnvireGraph::edge_iterator it = edges_it.first; it != edges_it.second; it++)
+    {
+        FrameId sourceNode;
+        sourceNode = getFrameId(source(*it, this->graph()));
+        
+        //std::cout << sourceNode << "->" << getFrameId(target(*it, this->graph())) << std::endl;
+
+        nodeMap[sourceNode]++;
+    }
+
+    //insert nodes with strictly one child in result
+    for(std::map<FrameId, unsigned int>::iterator it = nodeMap.begin(); it != nodeMap.end(); it++)
+    {
+        //std::cout << it->first << " : " << std::to_string(it->second - 1) << " children" << std::endl;
+        if(it->second == 1)
+        {
+            //if rootNode is !NULL remove rootNode from leaves (usefull if the root node has only one child)
+            if(!rootNode)
+            {
+                leafList.push_back(it->first);
+            }
+            else if(it->first != *rootNode)
+            {
+                leafList.push_back(it->first);
+            }
+        }
+    }
+
+    return leafList;
+}
